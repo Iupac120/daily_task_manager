2c97e21c02c52fc9ce82ee648a2513654f4ae999
Iupac120 <oforokpara@gmail.com>
2023-12-06 16:20:33 +0100

    updated db

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE DATABASE africanapublisher;
+CREATE DATABASE routinetaskmanager;
 
 -- Account Creation
 CREATE TABLE users (
@@ -8,15 +8,12 @@ CREATE TABLE users (
     user_name VARCHAR(255) NOT NULL,
     user_email VARCHAR(255) UNIQUE NOT NULL,
     user_password VARCHAR(255),
-    social_media VARCHAR(255),
     is_verified BOOLEAN DEFAULT FALSE,
     is_admin BOOLEAN DEFAULT FALSE,
     otp TEXT,
     otp_time TIMESTAMP,
     google_id VARCHAR(255),
     facebook_id VARCHAR(255),
-    two_factor_auth_enabled BOOLEAN,
-    data_privacy_location VARCHAR(255),
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -26,6 +23,5 @@ CREATE TABLE user_profiles (
     user_id INT PRIMARY KEY REFERENCES users(user_id),
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    avatar_url TEXT,
-    display_mode boolean DEFAULT TRUE
+    avatar_url TEXT
 );
